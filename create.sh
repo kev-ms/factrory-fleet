7ba958a2798c7ae96cb0c460cc1c9adff0637446
aflinchb <anflinch@microsoft.com>
2022-04-14 20:17:04 +0000

    kic fleet create

--- create.sh
+++ create.sh
@@ -6,3 +6,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 flt create --branch pg-fleet --gitops -g pg-fleet --ssl cseretail.com -c central-ar-lr-101
 flt create --branch pg-fleet --gitops -g pg-fleet --ssl cseretail.com -c east-tn-nashville-101
 flt create --branch pg-fleet --gitops -g pg-fleet --ssl cseretail.com -c west-wa-spokane-101
+flt create --gitops --ssl cseretail.com -g pg-fleet -c corp-monitoring-pg
