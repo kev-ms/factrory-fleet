4c1cb4af81f1c6fbd4f570a6933dd807ae47b5e2
Bart Robertson <bartr@microsoft.com>
2022-04-19 06:48:19 +0000

    updated scripts

--- delete.sh
+++ delete.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 flt delete central-tx-atx-501 &
-flt delete central-tx-atx-502 &
-flt delete central-tx-atx-503 &
-flt delete central-tx-atx-504 &
 
 rm ips
 
 az group delete -y --no-wait -g jofultz-fleet
 
 git pull
-git restore -s origin/main config deploy
-git commit -am "restore config and deploy"
+git commit -am "flt delete"
 git push
