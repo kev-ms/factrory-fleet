2a5255319b4b5bf4ab3cfc8a9341cc133cc68e9a
Bart Robertson <bartr@microsoft.com>
2022-04-19 06:34:35 +0000

    updated scripts

--- create.sh
+++ create.sh
@@ -3,5 +3,4 @@
 # change to this directory
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
-flt create --gitops --ssl cseretail.com -g jofultz-fleet -c central-tx-atx-501 &
-flt create --gitops --ssl cseretail.com -g jofultz-fleet -c central-tx-atx-502 &
+flt create --ssl cseretail.com -g jofultz-fleet -c central-tx-atx-501
