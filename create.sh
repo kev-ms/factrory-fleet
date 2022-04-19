1b8bfe577fe60409a02fbda1d2a756cc64b3486f
bartr <bartr@microsoft.com>
2022-04-19 05:53:57 +0000

    flt create

--- create.sh
+++ create.sh
@@ -4,3 +4,4 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 flt create --gitops --ssl cseretail.com -g jofultz-fleet -c central-tx-atx-501
+flt create --gitops --ssl cseretail.com -g jofultz-fleet -c central-tx-atx-502 --debug
