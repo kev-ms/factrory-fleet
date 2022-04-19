666ef08ba143fe04be0e23e06bc265e09048cf60
Bart Robertson <bartr@microsoft.com>
2022-04-19 07:00:12 +0000

    updated scripts

--- create.sh
+++ create.sh
@@ -3,4 +3,4 @@
 # change to this directory
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
-flt create --ssl cseretail.com -g jofultz-fleet -c central-tx-atx-501
+flt create --ssl cseretail.com --branch jofultz-fleet -g jofultz-fleet -c central-tx-atx-501
