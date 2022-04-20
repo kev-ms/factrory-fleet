40e6f2fc1a7f0410be02c1950a5855620ad1aff0
bartr <bartr@microsoft.com>
2022-04-20 00:57:22 +0000

    flt delete

--- delete.sh
+++ delete.sh
@@ -2,15 +2,15 @@
 
 echo "uncomment delete.sh commands to run it"
 
-# flt delete central-tx-atx-512 &
-# flt delete east-ga-atl-512 &
-# flt delete west-wa-sea-512 &
-# flt delete corp-monitoring-jofultz &
+flt delete central-tx-atx-512 &
+flt delete east-ga-atl-512 &
+flt delete west-wa-sea-512 &
+flt delete corp-monitoring-jofultz &
 
-# rm ips
+rm ips
 
-# az group delete -y --no-wait -g jofultz-fleet
+az group delete -y --no-wait -g jofultz-fleet
 
-# git pull
-# git commit -am "flt delete"
-# git push
+git pull
+git commit -am "flt delete"
+git push
