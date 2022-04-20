41fa9e83248f98934b07232ebc37b9be9f8d1ee7
bartr <bartr@microsoft.com>
2022-04-20 00:58:22 +0000

    updated scripts

--- create.sh
+++ create.sh
@@ -6,3 +6,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 flt create --ssl cseretail.com --branch jofultz-fleet -g jofultz-fleet -c central-tx-atx-512 &
 flt create --ssl cseretail.com --branch jofultz-fleet -g jofultz-fleet -c east-ga-atl-512 &
 flt create --ssl cseretail.com --branch jofultz-fleet -g jofultz-fleet -c west-wa-sea-512 &
+flt create --ssl cseretail.com --branch jofultz-fleet -g jofultz-fleet -c corp-monitoring-jofultz &
