e366fb444e87a6365be001e646df8d91be680127
bartr <bartr@microsoft.com>
2022-04-28 22:35:41 +0000

    updated scripts

--- delete.sh
+++ delete.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-flt delete central-ar-lr-101
-flt delete east-tn-nashville-101
-flt delete west-wa-spokane-101
+flt delete central-ar-lr-101 &
+flt delete east-tn-nashville-101 &
+flt delete west-wa-spokane-101 &
+flt delete corp-monitoring-pg &
 
 rm ips
 
