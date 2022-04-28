1cf3332bc7b349f8b65c8f7b97b941304d988687
bartr <bartr@microsoft.com>
2022-04-28 15:05:19 +0000

    updated scripts

--- create.sh
+++ create.sh
@@ -5,6 +5,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 az group create -l centralus -g micro-fleet -o table
 
-flt create --gitops --ssl cseretail.com -g micro-fleet -c central-tx-atx-m01
-flt create --gitops --ssl cseretail.com -g micro-fleet -c east-ga-atl-m01
-flt create --gitops --ssl cseretail.com -g micro-fleet -c west-wa-sea-m01
+flt create --gitops --ssl cseretail.com -g micro-fleet -c central-tx-atx-m01 &
+flt create --gitops --ssl cseretail.com -g micro-fleet -c east-ga-atl-m01 &
+flt create --gitops --ssl cseretail.com -g micro-fleet -c west-wa-sea-m01 &
