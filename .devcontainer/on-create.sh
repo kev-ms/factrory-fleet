2fb3efdfacf4329509524c72df44c07a2e87dbe0
bartr <bartr@microsoft.com>
2022-05-01 01:50:52 +0000

    updated Codespaces

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -90,5 +90,13 @@ cd ../inner-loop || exit
 kic cluster rebuild
 cd "$REPO_BASE" || exit
 
+# only run apt upgrade on pre-build
+if [ "$CODESPACE_NAME" = "null" ]
+then
+    echo "$(date +'%Y-%m-%d %H:%M:%S')    upgrading" >> "$HOME/status"
+    sudo apt-get update
+    sudo apt-get upgrade -y
+fi
+
 echo "on-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create complete" >> "$HOME/status"
