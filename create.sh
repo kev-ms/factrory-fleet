9e0ee6aa92ad0c9daaf79a547ebc713a044837eb
bartr <bartr@microsoft.com>
2022-05-09 17:57:07 +0000

    updated scripts

--- create.sh
+++ create.sh
@@ -3,9 +3,8 @@
 # change to this directory
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
-az group create -l centralus -g jofultz-fleet
-
-flt create --ssl cseretail.com --branch jofultz-fleet -g jofultz-fleet -c central-tx-atx-510 &
-flt create --ssl cseretail.com --branch jofultz-fleet -g jofultz-fleet -c east-ga-atl-510 &
-flt create --ssl cseretail.com --branch jofultz-fleet -g jofultz-fleet -c west-wa-sea-510 &
-flt create --ssl cseretail.com --branch jofultz-fleet -g jofultz-fleet -c corp-monitoring-jofultz &
+flt create --ssl cseretail.com --branch jofultz-fleet -g jofultz-fleet \
+    -c central-tx-atx-510 \
+    -c east-ga-atl-510 \
+    -c west-wa-sea-510 \
+    -c corp-monitoring-jofultz
